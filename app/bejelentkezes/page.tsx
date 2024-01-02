0589c23f2c87403f6ba10c6219860543ae147d8e
hassanad <hassanad94@gmail.com>
2024-01-02 11:05:56 +0100

    init qr

---
+++ app/bejelentkezes/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import React from 'react'
+
+
+const Page = () => {
+  return (
+	<div>
+		<Image src="/next.svg" alt="QR Kód a bejelentkezéshez" fill />
+	</div>
+  )
+}
+
+export default Page
