45a402094438b57c15a2bce56f8e8dbe421cd963
hassanad <hassanad94@gmail.com>
2024-01-10 09:11:48 +0100

    debug changes

--- utils/database.ts
+++ utils/database.ts
@@ -1,5 +1,16 @@
 import mysql from "mysql2";
 
+
+console.log(
+  {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "23306"),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  }
+)
+
 export const db = mysql
   .createPool({
     host: process.env.DB_HOST,
